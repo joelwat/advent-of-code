3009f3bfbfb131c1c8346accd863e7e511721ebd
Joel Watson <joelwat@gmail.com>
2022-01-02 22:54:53 -0800

    A few basic errors for the app

---
+++ 2021/day1/src/app_errors.rs
@@ -0,0 +1,10 @@
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+pub enum AppError {
+  #[error("Error opening file: {0}")]
+  IoError(#[from] std::io::Error),
+
+  #[error("File not found.")]
+  FileNotFound,
+}
