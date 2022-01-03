f390c59ac545ba5c5253fa02536a5e4757590d79
Joel Watson <joelwat@gmail.com>
2022-01-02 22:55:24 -0800

    Read in intergers from a file

---
+++ 2021/day1/src/file_reader.rs
@@ -0,0 +1,42 @@
+use std::{
+  fs::File,
+  io::{
+    BufRead,
+    BufReader,
+  },
+  path::Path,
+};
+
+use crate::app_errors::AppError;
+
+pub fn load_data() -> Result<Vec<u64>, AppError> {
+  if Path::new("src/input.txt").exists() {
+    let file = File::open("src/input.txt")?;
+
+    let mut reader = BufReader::new(file);
+    let mut line = String::new();
+    let mut data: Vec<u64> = Vec::new();
+
+    loop {
+      match reader.read_line(&mut line) {
+        Ok(bytes_read) => {
+          // Check for EOF condition
+          if bytes_read == 0 {
+            break;
+          }
+
+          data.push(line.trim().parse::<u64>().unwrap());
+
+          line.clear();
+        }
+        Err(err) => {
+          return Err(AppError::IoError(err));
+        }
+      }
+    }
+
+    return Ok(data)
+  }
+
+  Err(AppError::FileNotFound)
+}
