43a18bbcbefcf49d88fada025934649319c401bb
Joel Watson <joelwat@gmail.com>
2022-01-05 23:19:33 -0800

    Switch to run part 2

--- 2021/day1/src/main.rs
+++ 2021/day1/src/main.rs
@@ -1,9 +1,13 @@
 use std::process;
 
-use libday1::run;
+#[allow(unused_imports)]
+use libday1::{
+    run_part1,
+    run_part2,
+};
 
 fn main() {
-    if let Err(err) = run() {
+    if let Err(err) = run_part2() {
         eprintln!("Can I haz b0rken?!");
         eprintln!("{:#}", err);
 
