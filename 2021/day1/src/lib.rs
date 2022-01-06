8b74e2cd9fc508619e7728657925c0838d672efa
Joel Watson <joelwat@gmail.com>
2022-01-05 23:18:37 -0800

    Add a run function for day1 part 2

--- 2021/day1/src/lib.rs
+++ 2021/day1/src/lib.rs
@@ -1,9 +1,36 @@
 mod app_errors;
 mod file_reader;
+mod window;
 
 use app_errors::AppError;
+use window::Window;
 
-pub fn run() -> Result<(), AppError> {
+pub fn run_part2() -> Result<(), AppError> {
+  let data = file_reader::load_data()?;
+  let mut increased: usize = 0;
+  let mut window = Window::new();
+  let mut cur_sum: u64;
+  let mut prev_sum: u64;
+
+  for depth in &data {
+    window.slide(depth);
+
+    if window.is_ready() {
+      cur_sum = window.get_current_sum();
+      prev_sum = window.get_previous_sum();
+
+      if cur_sum > prev_sum {
+        increased += 1;
+      }
+    }
+  }
+
+  println!("There are {} measurements larger than the previous", increased);
+
+  Ok(())
+}
+
+pub fn run_part1() -> Result<(), AppError> {
   let data = file_reader::load_data()?;
   let mut increased: usize = 0;
   let mut prev: Option<&u64> = None;
