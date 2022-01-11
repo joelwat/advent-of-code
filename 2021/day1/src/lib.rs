a726bc4be34eaca0fda39343cb3ff292df780fbb
Joel Watson <joelwat@gmail.com>
2022-01-10 21:25:32 -0800

    Move app_error and file_reader to utils crate

--- 2021/day1/src/lib.rs
+++ 2021/day1/src/lib.rs
@@ -1,56 +1,61 @@
-mod app_errors;
-mod file_reader;
 mod window;
 
-use app_errors::AppError;
+use utils::{app_errors::AppError, file_reader::load_data};
+
 use window::Window;
 
 pub fn run_part2() -> Result<(), AppError> {
-  let data = file_reader::load_data()?;
-  let mut increased: usize = 0;
-  let mut window = Window::new();
-  let mut cur_sum: u64;
-  let mut prev_sum: u64;
+    let data = load_data()?;
+    let mut increased: usize = 0;
+    let mut window = Window::new();
+    let mut cur_sum: u64;
+    let mut prev_sum: u64;
 
-  for depth in &data {
-    window.slide(depth);
+    for depth in &data {
+        window.slide(depth);
 
-    if window.is_ready() {
-      cur_sum = window.get_current_sum();
-      prev_sum = window.get_previous_sum();
+        if window.is_ready() {
+            cur_sum = window.get_current_sum();
+            prev_sum = window.get_previous_sum();
 
-      if cur_sum > prev_sum {
-        increased += 1;
-      }
+            if cur_sum > prev_sum {
+                increased += 1;
+            }
+        }
     }
-  }
 
-  println!("There are {} measurements larger than the previous", increased);
+    println!(
+        "There are {} measurements larger than the previous",
+        increased
+    );
 
-  Ok(())
+    Ok(())
 }
 
 pub fn run_part1() -> Result<(), AppError> {
-  let data = file_reader::load_data()?;
-  let mut increased: usize = 0;
-  let mut prev: Option<&u64> = None;
+    let data = file_reader::load_data()?;
+    let mut increased: usize = 0;
+    let mut prev: Option<&u64> = None;
 
-  for depth in &data {
-    // Check if this is the first iteration
-    if prev.is_none() {
-      prev = Some(depth);
+    for depth in &data {
+        // Check if this is the first iteration
+        if prev.is_none() {
+            prev = Some(depth);
 
-      continue;
+            continue;
+        }
+
+        if depth > prev.unwrap() {
+            increased += 1;
+        }
+
+        prev = Some(depth);
     }
 
-    if depth > prev.unwrap() {
-      increased += 1;
-    }
+    println!(
+        "There are {} measurements larger than the previous",
+        increased
+    );
 
-    prev = Some(depth);
-  }
-
-  println!("There are {} measurements larger than the previous", increased);
-
-  Ok(())
+    Ok(())
 }
---
+++ utils/src/lib.rs
@@ -0,0 +1,2 @@
+pub mod app_errors;
+pub mod file_reader;
