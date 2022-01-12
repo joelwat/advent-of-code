703da98988ecc3daf431dee042067111d353bc38
Joel Watson <joelwat@gmail.com>
2022-01-11 17:12:51 -0800

    Lint: formatting

--- utils/src/app_errors.rs
+++ utils/src/app_errors.rs
@@ -2,9 +2,9 @@ use thiserror::Error;
 
 #[derive(Debug, Error)]
 pub enum AppError {
-  #[error("Error opening file: {0}")]
-  IoError(#[from] std::io::Error),
+    #[error("Error opening file: {0}")]
+    IoError(#[from] std::io::Error),
 
-  #[error("File not found.")]
-  FileNotFound,
+    #[error("File not found.")]
+    FileNotFound,
 }
--- utils/src/file_reader.rs
+++ utils/src/file_reader.rs
@@ -1,42 +1,39 @@
 use std::{
-  fs::File,
-  io::{
-    BufRead,
-    BufReader,
-  },
-  path::Path,
+    fs::File,
+    io::{BufRead, BufReader},
+    path::Path,
 };
 
 use crate::app_errors::AppError;
 
 pub fn load_data() -> Result<Vec<u64>, AppError> {
-  if Path::new("src/input.txt").exists() {
-    let file = File::open("src/input.txt")?;
+    if Path::new("src/input.txt").exists() {
+        let file = File::open("src/input.txt")?;
 
-    let mut reader = BufReader::new(file);
-    let mut line = String::new();
-    let mut data: Vec<u64> = Vec::new();
+        let mut reader = BufReader::new(file);
+        let mut line = String::new();
+        let mut data: Vec<u64> = Vec::new();
 
-    loop {
-      match reader.read_line(&mut line) {
-        Ok(bytes_read) => {
-          // Check for EOF condition
-          if bytes_read == 0 {
-            break;
-          }
+        loop {
+            match reader.read_line(&mut line) {
+                Ok(bytes_read) => {
+                    // Check for EOF condition
+                    if bytes_read == 0 {
+                        break;
+                    }
 
-          data.push(line.trim().parse::<u64>().unwrap());
+                    data.push(line.trim().parse::<u64>().unwrap());
 
-          line.clear();
+                    line.clear();
+                }
+                Err(err) => {
+                    return Err(AppError::IoError(err));
+                }
+            }
         }
-        Err(err) => {
-          return Err(AppError::IoError(err));
-        }
-      }
+
+        return Ok(data);
     }
 
-    return Ok(data)
-  }
-
-  Err(AppError::FileNotFound)
+    Err(AppError::FileNotFound)
 }
