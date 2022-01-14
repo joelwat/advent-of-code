a309b0d39ebf3f0803b296248d544f5e97003e0f
Joel Watson <joelwat@gmail.com>
2022-01-14 14:41:54 -0800

    Complete day2 - part 2

--- 2021/day2/src/lib.rs
+++ 2021/day2/src/lib.rs
@@ -33,6 +33,24 @@ pub fn run_part1() -> Result<(), AppError> {
 }
 
 pub fn run_part2() -> Result<(), AppError> {
+    let mut aim: u64 = 0;
+    let mut depth: u64 = 0;
+    let mut distance: u64 = 0;
+
+    let file_path = Path::new("2021/day2/input");
+
+    let file_data = load_data::<SubCommand>(file_path, &parse_command)?;
+
+    for line in file_data {
+        handle_command_part2(line, &mut depth, &mut distance, &mut aim);
+    }
+
+    println!("Depth: {}", depth);
+    println!("Distance: {}", distance);
+    println!();
+
+    println!("Puzzle answer: {}", depth * distance);
+
     Ok(())
 }
 
@@ -49,5 +67,16 @@ fn handle_command(command: SubCommand, depth: &mut u64, distance: &mut u64) {
         Command::Down(val) => *depth += val,
         Command::Forward(val) => *distance += val,
         Command::Up(val) => *depth -= val,
+    }
+}
+
+fn handle_command_part2(command: SubCommand, depth: &mut u64, distance: &mut u64, aim: &mut u64) {
+    match command.0 {
+        Command::Down(x) => *aim += x,
+        Command::Forward(x) => {
+            *distance += x;
+            *depth += *aim * x;
+        },
+        Command::Up(x) => *aim -= x,
     }
 }
--- 2021/day2/src/main.rs
+++ 2021/day2/src/main.rs
@@ -1,10 +1,13 @@
 use std::process;
 
 #[allow(unused_imports)]
-use libday2::{run_part1, run_part2};
+use libday2::{
+    run_part1,
+    run_part2,
+};
 
 fn main() {
-    if let Err(err) = run_part1() {
+    if let Err(err) = run_part2() {
         eprintln!("Can I haz b0rken?!");
         eprintln!("{:#}", err);
 
