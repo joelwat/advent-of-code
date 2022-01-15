a3812aaaaee4f071c8f341207ad92657c47e7fc8
Joel Watson <joelwat@gmail.com>
2022-01-14 17:06:35 -0800

    clippy::needless-pass-by-value
    SubCommand is passed by value, but not consumed in the function body.
    Consider taking a reference instead: `&SubCommand`

--- 2021/day2/src/lib.rs
+++ 2021/day2/src/lib.rs
@@ -20,7 +20,7 @@ pub fn run_part1() -> Result<(), AppError> {
     let file_data = load_data::<SubCommand>(file_path, &parse_command)?;
 
     for line in file_data {
-        handle_command(line, &mut depth, &mut distance);
+        handle_command(&line, &mut depth, &mut distance);
     }
 
     println!("Depth: {}", depth);
@@ -42,7 +42,7 @@ pub fn run_part2() -> Result<(), AppError> {
     let file_data = load_data::<SubCommand>(file_path, &parse_command)?;
 
     for line in file_data {
-        handle_command_part2(line, &mut depth, &mut distance, &mut aim);
+        handle_command_part2(&line, &mut depth, &mut distance, &mut aim);
     }
 
     println!("Depth: {}", depth);
@@ -62,7 +62,7 @@ pub fn parse_command(str: &str) -> Result<SubCommand, AppError> {
     Ok(parsed)
 }
 
-fn handle_command(command: SubCommand, depth: &mut u64, distance: &mut u64) {
+fn handle_command(command: &SubCommand, depth: &mut u64, distance: &mut u64) {
     match command.0 {
         Command::Down(val) => *depth += val,
         Command::Forward(val) => *distance += val,
@@ -70,7 +70,7 @@ fn handle_command(command: SubCommand, depth: &mut u64, distance: &mut u64) {
     }
 }
 
-fn handle_command_part2(command: SubCommand, depth: &mut u64, distance: &mut u64, aim: &mut u64) {
+fn handle_command_part2(command: &SubCommand, depth: &mut u64, distance: &mut u64, aim: &mut u64) {
     match command.0 {
         Command::Down(x) => *aim += x,
         Command::Forward(x) => {
